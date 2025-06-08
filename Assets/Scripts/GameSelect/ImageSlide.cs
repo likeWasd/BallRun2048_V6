df6818bf2775bec3e8e21565c5016a616b7d031c
likeWasd <mana.dog0077@gmail.com>
2025-06-08 13:14:16 +0900

    DOtweenを使って滑らかに移動できるようにした
    ・次回はスタートのボタンの処理を作る

--- Assets/Scripts/GameSelect/ImageSlide.cs
+++ Assets/Scripts/GameSelect/ImageSlide.cs
@@ -1,19 +1,56 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class ImageSlide : MonoBehaviour
 {
     [SerializeField] RectTransform tallManRunRT;
+    [SerializeField] int nowSelectedGame;
+    public int maxGameNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        nowSelectedGame = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            tallManRunRT.anchoredPosition += new Vector2(10f, 0f);
+        //tallManRunRT.anchoredPosition += new Vector2(10f, 0f);
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            leftPressed();
         }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rightPressed();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            startPressed();
+        }
+    }
+
+    public void leftPressed()
+    {
+        if (nowSelectedGame > 1) {
+            nowSelectedGame--;
+            tallManRunRT.DOComplete();
+            tallManRunRT.DOAnchorPosX(600f, 1f).SetRelative(true);
+        }
+    }
+
+    public void rightPressed()
+    {
+        if (nowSelectedGame < maxGameNumber)
+        {
+            nowSelectedGame++;
+            tallManRunRT.DOComplete();
+            tallManRunRT.DOAnchorPosX(-600f, 1f).SetRelative(true);
+        }
+    }
+
+    public void startPressed()
+    {
+
     }
 }
